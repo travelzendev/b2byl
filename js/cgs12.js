825bd31af45893cddfd6d4e993affce81653453a
aufula <aufula@hotmail.com>
2013-02-28 18:14:03 +0800

    'update'

--- js/cgs12.js
+++ js/cgs12.js
@@ -1,6 +1,11 @@
 head.ready(function(){
 
+    $.extend(order_info_psr, {
+        count_psr:0
+    });
     var viewModel = kendo.observable(order_info_psr);
+
+    window.pviewModel = viewModel;
 
     var passenger_info_tpl = kendo.template($('#passenger_info_wrap_tmpl').html());
     $('#passenger_info').append(passenger_info_tpl({room_info:viewModel.room_info}));
